f8350990257f562381db0b27f4e4129fe01662bd
Fernando Campos <ferchoeth@gmail.com>
2023-02-01 17:48:10 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,134 @@
+import { operate } from './utilities.js';
+
+function displayOperation(numbers, operator, symbol, resultOfOperation) {
+    const displayText = document.getElementById('text-display');
+    if(
+        symbol === '+' || symbol === '-' ||
+        symbol === 'x' || symbol === '/' ||
+        symbol === '=' || symbol === 'C'
+    ) {
+        displayText.classList.add('processing');
+        setTimeout(() => {
+            if(resultOfOperation) {
+                displayText.innerText = resultOfOperation;
+            } else if(symbol === 'C') {
+                displayText.innerText = '0';
+            } else {
+                displayText.innerText = numbers[0];
+            }
+            displayText.classList.remove('processing');
+        }, 100)
+    } else if(!operator) {
+        displayText.innerText = numbers[0];
+    } else if(operator) {
+        displayText.innerText = numbers[1];
+    } else if(resultOfOperation) {
+        displayText.innerText = resultOfOperation;
+    }
+};
+
+function operationHandler() {
+    const numbers = ['', ''];
+    let operator = '';
+    let resultOfOperation = null;
+    return (symbol) => {
+        try {
+            switch(symbol) {
+                case '+':
+                    if(operator) {
+                        resultOfOperation =+ operate(numbers[0] === '' ? resultOfOperation : parseInt(numbers[0]), parseInt(numbers[1]), operator);
+                        numbers[0] = resultOfOperation;
+                        numbers[1] = '';
+                        operator = 'add';
+                        console.log(resultOfOperation);
+                        displayOperation(numbers, operator, symbol, resultOfOperation);
+                        break;
+                    }
+                    operator = 'add';
+                    displayOperation(numbers, operator, symbol, resultOfOperation);
+                    break;
+                case '-':
+                    if(operator) {
+                        resultOfOperation =+ operate(numbers[0] === '' ? resultOfOperation : parseInt(numbers[0]), parseInt(numbers[1]), operator);
+                        numbers[0] = resultOfOperation;
+                        numbers[1] = '';
+                        operator = 'substract';
+                        console.log(resultOfOperation);
+                        displayOperation(numbers, operator, symbol, resultOfOperation);
+                        break;
+                    }
+                    operator = 'substract';
+                    displayOperation(numbers, operator, symbol, resultOfOperation);
+                    break;
+                case 'x':
+                    if(operator) {
+                        resultOfOperation =+ operate(numbers[0] === '' ? resultOfOperation : parseInt(numbers[0]), parseInt(numbers[1]), operator);
+                        numbers[0] = resultOfOperation;
+                        numbers[1] = '';
+                        operator = 'multiply';
+                        console.log(resultOfOperation);
+                        displayOperation(numbers, operator, symbol, resultOfOperation);
+                        break;
+                    }
+                    operator = 'multiply';
+                    displayOperation(numbers, operator, symbol, resultOfOperation);
+                    break;
+                case '/':
+                    if(operator) {
+                        resultOfOperation =+ operate(numbers[0] === '' ? resultOfOperation : parseInt(numbers[0]), parseInt(numbers[1]), operator);
+                        numbers[0] = resultOfOperation;
+                        numbers[1] = '';
+                        operator = 'divide';
+                        console.log(resultOfOperation);
+                        displayOperation(numbers, operator, symbol, resultOfOperation);
+                        break;
+                    }
+                    operator = 'divide';
+                    displayOperation(numbers, operator, symbol, resultOfOperation);
+                    break;
+                case '=':
+                    resultOfOperation =+ operate(numbers[0] === '' ? resultOfOperation : parseInt(numbers[0]), parseInt(numbers[1]), operator);
+                    numbers[0] = '';
+                    numbers[1] = '';
+                    operator = '';
+                    console.log(resultOfOperation);
+                    displayOperation(numbers, operator, symbol, resultOfOperation);
+                    break;
+                case 'C':
+                    numbers[0] = '';
+                    numbers[1] = '';
+                    operator = '';
+                    resultOfOperation = null;
+                    displayOperation(numbers, operator, symbol, resultOfOperation);
+                    break;
+                default:
+                    if(operator !== '') {
+                        numbers[1] += symbol;
+                    } else {
+                        numbers[0] += symbol;
+                    }
+                    displayOperation(numbers, operator, symbol, resultOfOperation);
+                    break;
+            }
+        } catch(error) {
+            alert(`
+                Ups, something went wrong.
+                ${error}
+            `);
+            numbers[0] = '';
+            numbers[1] = '';
+            operator = '';
+            resultOfOperation = null;
+            displayOperation(numbers, operator, 'C', resultOfOperation);
+        }
+    }
+};
+
+const operation = operationHandler();
+
+const buttons = document.querySelectorAll('.calc-button');
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        operation(e.target.innerText);
+    });
+});
---
+++ utilities.js
@@ -0,0 +1,35 @@
+function add(a,b) {
+    return a + b;
+};
+function substract(a,b) {
+    return a - b;
+};
+function multiply(a,b) {
+    return (a * b).toFixed(2);
+};
+function divide(a,b) {
+    if(b === 0) {
+        throw new Error("Can't divide by zero")
+    }
+    return (a / b).toFixed(2);
+};
+function operate(a,b,operator) {
+    try {
+        switch(operator) {
+            case 'add':
+                return add(a,b);
+            case 'substract':
+                return substract(a,b);
+            case 'multiply':
+                return multiply(a,b);
+            case 'divide':
+                return divide(a,b);
+            default:
+                throw new Error('No valid operator');
+        }
+    } catch(error) {
+        throw new Error(error);
+    }
+}
+
+export { operate };
